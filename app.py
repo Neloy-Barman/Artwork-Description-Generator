1df27d54ff8020015b942af1dd92c91e36a28428
Neloy Barman <neloycareer018@gmail.com>
2024-03-24 23:05:22 +0600

    Classification html added

--- app.py
+++ app.py
@@ -5,7 +5,8 @@ app = Flask(__name__)
 
 @app.route("/", methods = ['GET', 'POST'])
 def index():
-    return render_template('index.html')
+    # return render_template('index.html')
+    return render_template('result.html')
 
 
 if __name__ == "__main__":
